a31c1dd31144458b2ada4ad67468d87649c9c9c9
singhsansun <46861530+singhsansun@users.noreply.github.com>
2019-03-31 15:39:10 +0200

    Update fs_online_sources.meta.js

--- fs_online_sources.meta.js
+++ fs_online_sources.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Familysearch source adder
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1.1.1
+// @version      1
 // @author       singhsansun
 // @description  Quickly add external online sources to FamilySearch profiles.
 // @homepage     https://github.com/singhsansun/fs
@@ -9,4 +9,3 @@
 // @downloadURL  https://raw.githubusercontent.com/singhsansun/fs/master/fs_online_sources.user.js
 // @match        https://www.familysearch.org/tree/person*
 // @grant        none
-// ==/UserScript==
